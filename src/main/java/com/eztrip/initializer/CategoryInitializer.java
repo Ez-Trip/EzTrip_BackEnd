7d64064fa72fe581de55c1b0b346373cc5cdb1f7
yjs <wptmd97@naver.com>
2024-10-02 01:21:18 +0900

    edit: check category list in database condition

--- src/main/java/com/eztrip/initializer/CategoryInitializer.java
+++ src/main/java/com/eztrip/initializer/CategoryInitializer.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 import java.util.Arrays;
 import java.util.List;
 
+// 카테고리 디비에서 조회 후 저장하는 부분
 @Component
 @RequiredArgsConstructor
 public class CategoryInitializer {
@@ -18,16 +19,16 @@ public class CategoryInitializer {
     @PostConstruct
     public void init() {
         List<Category> categories = Arrays.asList(
-                new Category("A1", "Category A1"),
-                new Category("A2", "Category A2"),
-                new Category("A3", "Category A3"),
-                new Category("A4", "Category A4"),
-                new Category("B1", "Category B1"),
-                new Category("B2", "Category B2"),
-                new Category("B3", "Category B3"),
-                new Category("C1", "Category C1"),
-                new Category("D1", "Category D1"),
-                new Category("E1", "Category E1")
+                new Category("A1", "Category A1"), //한식
+                new Category("A2", "Category A2"), //중식
+                new Category("A3", "Category A3"), //일식
+                new Category("A4", "Category A4"), //양식
+                new Category("B1", "Category B1"), //호텔
+                new Category("B2", "Category B2"), //모텔
+                new Category("B3", "Category B3"), //게스트하우스
+                new Category("C1", "Category C1"), //카페
+                new Category("D1", "Category D1"), //문화시설
+                new Category("E1", "Category E1") //관광시설
         );
 
         for (Category category : categories) {
--- src/main/java/com/eztrip/service/member/MemberService.java
+++ src/main/java/com/eztrip/service/member/MemberService.java
@@ -76,7 +76,7 @@ public class MemberService {
 
             memberCategoryService.createMemberCategory(joinMember, category, i+1);
         }
-
+        // 카테고리 디비에서 조회 후 저장하는 부분
         memberRepository.save(joinMember);
     }
 
@@ -133,18 +133,9 @@ public class MemberService {
 
         Member findMember = findById(id);
 
-        /*
-        여기서 생기는 궁금증 ??
-        - DB에 save를 안했는데 어떻게 끝나요 이게
-        - 지금 해준 작업은 id로 회원 객체를 찾았어 -> member field update
-        - db update X
-        - JPA 변경감지
-         */
         findMember.update(updateDto);
     }
 
-
-    // 카테고리 디비에서 조회 후 저장하는 부분
 
 
 }
